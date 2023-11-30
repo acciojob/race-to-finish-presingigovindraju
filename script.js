3a0c61d883cb44bf161ac9e67877c196bffb98f5
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-11-30 10:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-window.promises = [];
+// Add your promises to the array promises
+window.promises = [
+  new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 3 resolved"), Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 4 resolved"), Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 5 resolved"), Math.random() * 5000 + 1000)),
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Use Promise.any to wait for the first promise to resolve
+Promise.any(window.promises)
+  .then(result => {
+    // Print the result to the div with id "output"
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
+  })
+  .catch(error => console.error(error));
